e2c9c7050a07b6bd0931805ebe1b7333980bb41a
Jacob McFaul <mcjakedude@gmail.com>
2019-06-12 10:54:06 -0700

    insert contains and get-max are done I think

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,13 +5,45 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+      if value < self.value:
+          if not self.left:
+              self.left = BinarySearchTree(value)
+          else:
+              self.left.insert(value)
+      elif value >= self.value:
+          if self.right is None:
+              self.right = BinarySearchTree(value)
+          else:
+              self.right.insert(value)
 
   def contains(self, target):
-    pass
+        if target < self.value:
+            if self.left is None:
+                return False
+            return self.left.contains(target)
+        elif target > self.value:
+            if self.right is None:
+                return False
+            return self.right.contains(target)
+        else:
+            return True
 
   def get_max(self):
-    pass
+    current = self
+    while current.right:
+      current = current.right
+    return current.value
 
   def for_each(self, cb):
     pass
+
+
+
+root = BinarySearchTree(50)
+root.insert(5)
+root.insert(68)
+root.insert(72)
+root.insert(20)
+root.insert(29)
+root.contains(49)
+root.get_max()
